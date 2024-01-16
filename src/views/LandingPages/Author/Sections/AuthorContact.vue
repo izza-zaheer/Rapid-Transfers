3f5ecf2b0eed5e8c1220e250b74d3aaf26f3654a
izaheer <izaheer@teresol.com>
2024-01-16 11:16:40 +0500

    first commit

---
+++ src/views/LandingPages/Author/Sections/AuthorContact.vue
@@ -0,0 +1,231 @@
+<script setup>
+import { onMounted, ref } from "vue";
+//Vue Material Kit 2 components
+import MaterialButton from "@/components/MaterialButton.vue";
+import MaterialInput from "@/components/MaterialInput.vue";
+import MaterialTextArea from "@/components/MaterialTextArea.vue";
+import axios from "axios";
+
+// image
+import bgContact from "@/assets/img/examples/blog2.jpg";
+
+// tooltip
+import setTooltip from "@/assets/js/tooltip";
+
+// store
+import { useAppStore } from "@/stores";
+const store = useAppStore();
+// Reactive variables for form data
+const formData = ref({
+  name: "",
+  email: "",
+  mobileNo: "",
+  message: "",
+  EmailRequired: true,
+});
+onMounted(() => {
+  setTooltip(store.bootstrap);
+});
+const isFormDataValid = (formData) => {
+  return (
+    formData.name?.trim() !== "" ||
+    formData.email?.trim() !== "" ||
+    formData.mobileNo?.trim() !== ""
+  );
+};
+const saveName = async (val) => {
+  formData.name = val;
+};
+const saveEmail = async (val) => {
+  formData.email = val;
+};
+const saveMobileNo = async (val) => {
+  formData.mobileNo = val;
+};
+const saveMsg = async (val) => {
+  formData.msg = val;
+};
+// Define the submit method
+
+const submitForm = async () => {
+  debugger;
+  console.log(formData);
+  let validation = !isFormDataValid(formData.value);
+  if (validation) {
+    // alert("Please fill in all required(*) fields.");
+    return;
+  } else {
+    alert("form submitted");
+  }
+  try {
+    // Assuming you have a backend API endpoint for handling form submissions
+    // const response = await axios.post(
+    //   "http://localhost:3000/submit-form",
+    //   formData
+    // );
+    // Handle the response as needed
+    // console.log(response.data);
+  } catch (error) {
+    console.error("Error submitting form:", error);
+  }
+};
+</script>
+<template>
+  <section class="py-lg-5">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="card box-shadow-xl overflow-hidden mb-5">
+            <div class="row">
+              <div
+                class="col-lg-5 position-relative bg-cover px-0"
+                :style="{ backgroundImage: `url(${bgContact})` }"
+                loading="lazy"
+              >
+                <div
+                  class="z-index-2 text-center d-flex h-100 w-100 d-flex m-auto justify-content-center"
+                >
+                  <div class="mask bg-gradient-dark opacity-8"></div>
+                  <div class="p-5 ps-sm-8 position-relative text-start my-auto z-index-2">
+                    <h3 class="text-white">Contact Information</h3>
+                    <p class="text-white opacity-8 mb-4">
+                      Fill up the form and our Team will get back to you within
+                      24 hours.
+                    </p>
+                    <div class="d-flex p-2 text-white">
+                      <div>
+                        <i class="fas fa-phone text-sm"></i>
+                      </div>
+                      <div class="ps-3">
+                        <span class="text-sm opacity-8">(+34) 661 16 03 02</span>
+                      </div>
+                    </div>
+                    <div class="d-flex p-2 text-white">
+                      <div>
+                        <i class="fas fa-envelope text-sm"></i>
+                      </div>
+                      <div class="ps-3">
+                        <span class="text-sm opacity-8">rapidtransfers886@gmail.com</span>
+                      </div>
+                    </div>
+                    <div class="d-flex p-2 text-white">
+                      <div>
+                        <i class="fas fa-map-marker-alt text-sm"></i>
+                      </div>
+                      <div class="ps-3">
+                        <span class="text-sm opacity-8">Barcelona, Spain.</span>
+                      </div>
+                    </div>
+                    <div class="mt-4">
+                      <MaterialButton
+                        color="none"
+                        size="lg"
+                        class="btn-icon-only btn-link text-white mb-0"
+                        data-toggle="tooltip"
+                        data-placement="bottom"
+                        data-original-title="Log in with Facebook"
+                      >
+                        <i class="fab fa-facebook"></i>
+                      </MaterialButton>
+                      <MaterialButton
+                        color="none"
+                        size="lg"
+                        class="btn-icon-only btn-link text-white mb-0"
+                        data-toggle="tooltip"
+                        data-placement="bottom"
+                        data-original-title="Log in with Twitter"
+                      >
+                        <i class="fab fa-twitter"></i>
+                      </MaterialButton>
+                      <MaterialButton
+                        color="none"
+                        size="lg"
+                        class="btn-icon-only btn-link text-white mb-0"
+                        data-toggle="tooltip"
+                        data-placement="bottom"
+                        data-original-title="Log in with Dribbble"
+                      >
+                        <i class="fab fa-dribbble"></i>
+                      </MaterialButton>
+                      <MaterialButton
+                        color="none"
+                        size="lg"
+                        class="btn-icon-only btn-link text-white mb-0"
+                        data-toggle="tooltip"
+                        data-placement="bottom"
+                        data-original-title="Log in with Instagram"
+                      >
+                        <i class="fab fa-instagram"></i>
+                      </MaterialButton>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-7">
+                <form class="p-3" id="contact-form" method="post">
+                  <div class="card-header px-4 py-sm-5 py-3">
+                    <h2>Say Hi!</h2>
+                    <p class="lead">We'd like to talk with you.</p>
+                  </div>
+                  <div class="card-body pt-1">
+                    <div class="row">
+                      <div class="col-md-12 pe-2 mb-3">
+                        <MaterialInput
+                          class="input-group-static mb-4"
+                          label="My name is"
+                          type="text"
+                          :isRequired="true"
+                          placeholder="Full Name"
+                          @blur="saveName"
+                        />
+                      </div>
+                      <div class="col-md-12 pe-2 mb-3">
+                        <MaterialInput
+                          class="input-group-static mb-4"
+                          label="Email *"
+                          type="text"
+                          :isRequired="true"
+                          placeholder="abc@gmail.com"
+                          @blur="saveEmail"
+                        />
+                      </div>
+                      <div class="col-md-12 pe-2 mb-3">
+                        <MaterialInput
+                          class="input-group-static mb-4"
+                          label="Mobile Number"
+                          type="number"
+                          :isRequired="true"
+                          placeholder="+34 123 45 67 89"
+                          @blur="saveMobileNo"
+                        />
+                      </div>
+                      <div class="col-md-12 pe-2 mb-3">
+                        <MaterialTextArea
+                          class="input-group-static mb-4"
+                          placeholder="I want to say that..."
+                          :rows="6"
+                          @blur="saveMsg"
+                        >Your message</MaterialTextArea>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-md-6 text-end ms-auto">
+                        <MaterialButton
+                          variant="gradient"
+                          color="success"
+                          type="submit"
+                          class="mb-0"
+                          @submit.prevent="submitForm"
+                        >Send Message</MaterialButton>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ src/views/Presentation/Sections/PresentationInformation.vue
@@ -0,0 +1,65 @@
+<script setup>
+import RotatingCard from "../../../examples/cards/rotatingCards/RotatingCard.vue";
+import RotatingCardFront from "../../../examples/cards/rotatingCards/RotatingCardFront.vue";
+import RotatingCardBack from "../../../examples/cards/rotatingCards/RotatingCardBack.vue";
+import DefaultInfoCard from "../../../examples/cards/infoCards/DefaultInfoCard.vue";
+</script>
+<template>
+  <section class="my-5 py-5">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-4 ms-auto me-auto p-lg-4 mt-lg-0 mt-4">
+          <RotatingCard>
+            <RotatingCardFront
+              image="https://images.unsplash.com/photo-1569683795645-b62e50fbf103?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+              icon="touch_app"
+              title="Rapid <br /> Transfers"
+              description="We are here to serve you 24/7. Feel free to Book your ride anytime."
+            />
+
+            <RotatingCardBack
+              image="https://images.unsplash.com/photo-1498889444388-e67ea62c464b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1365&q=80"
+              title="Know about Us."
+              description="Rapid Transfer is your ideal choice for long-distance travel and our 24/7 travel agents are always available to make your trip hassle-free. Do you want to book in advance? No problem, we've got your back! Trust us, you won't want to take a trip without us"
+              :action="[
+                {
+                  route: './/sections/page-sections/hero-sections.html',
+                  label: 'Start with Us.',
+                },
+              ]"
+            />
+          </RotatingCard>
+        </div>
+        <div class="col-lg-6 ms-auto">
+          <div class="row justify-content-start">
+            <DefaultInfoCard
+              icon="content_copy"
+              title="A long trip? no problem!"
+              description="Our expertise lies in long-distance travel, and we love it! Are you planning a trip across the country? You can trust us! Do you need transportation to the airport? We've got you covered too!"
+            />
+            <DefaultInfoCard
+              icon="flip_to_front"
+              title="Book it in advance, change it later"
+              description="You are in control of your trip! Book it now or schedule it for later, make changes until the last minute. Just contact us via chat anytime, 24/7, and our travel agents will take care of your requests!"
+            />
+          </div>
+          <div class="row justify-content-start mt-5">
+            <DefaultInfoCard
+              class="mt-3"
+              icon="price_change"
+              title="We are here for you 24/7, no matter what."
+              description="Our travel agents are available via chat throughout the day and will quickly come to your aid before and after your trip, in less than a minute!"
+            />
+            <!-- <DefaultInfoCard
+              class="mt-3"
+              icon="devices"
+              title="Fully Responsive"
+              description="Regardless of the screen size, the website content will
+                  naturally fit the given resolution."
+            /> -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
